7e59ee71e737c2cf5c560641ec3b76ce8156f20f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-11-14 16:16:00 +0200

    Add pressable item functionality

--- RNCourse/components/GoalItem.js
+++ RNCourse/components/GoalItem.js
@@ -2,24 +2,31 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 export default function GoalItem({ text, id, onDeleteItem }) {
     return (
-        <Pressable onPress={onDeleteItem.bind(this, id)}>
-            <View style={styles.goalItem}>
+        <View style={styles.goalItem}>
+            <Pressable
+                android_ripple={{ color: '#210644' }}
+                onPress={onDeleteItem.bind(this, id)}
+                style={({ pressed }) => pressed && styles.pressedItem}
+            >
                 <Text style={styles.goalText}>
                     {text}
                 </Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View >
     );
 }
 
 const styles = StyleSheet.create({
     goalItem: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
         backgroundColor: '#5e0acc',
     },
+    pressedItem: {
+        opacity: 0.5
+    },
     goalText: {
-        color: 'white'
+        color: 'white',
+        padding: 8
     }
 });
