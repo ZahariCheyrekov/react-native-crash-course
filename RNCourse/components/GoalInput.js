4eceaf79f157ff986cdcb7a27dfb93d72bfb6e76
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-11-14 16:38:40 +0200

    Fix goal input component style

--- RNCourse/components/GoalInput.js
+++ RNCourse/components/GoalInput.js
@@ -23,7 +23,14 @@ export default function GoalInput({ addGoalHandler, visible }) {
                     value={enteredGoalText}
                 >
                 </TextInput>
-                <Button title='Add Goal' onPress={addGoalComponentHandler} />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title='Add Goal' onPress={addGoalComponentHandler} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title='Cancel' />
+                    </View>
+                </View>
             </View>
         </Modal>
     );
@@ -32,18 +39,25 @@ export default function GoalInput({ addGoalHandler, visible }) {
 const styles = StyleSheet.create({
     inputContainer: {
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         alignItems: 'center',
         marginBottom: 24,
+        padding: 16,
         borderBottomWidth: 1,
         borderColor: '#cccccc'
     },
     textInput: {
         borderWidth: 1,
         borderColor: '#cccccc',
-        width: '70%',
-        marginRight: 8,
+        width: '100%',
         padding: 8
     },
+    buttonContainer: {
+        marginTop: 16,
+        flexDirection: 'row'
+    },
+    button: {
+        width: 100,
+        marginHorizontal: 8
+    }
 });
