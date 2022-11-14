3e40ff505fc5cf26d297b4dd66ee730bc8b01ae3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-11-14 17:09:39 +0200

    Fix button elements

--- RNCourse/components/GoalInput.js
+++ RNCourse/components/GoalInput.js
@@ -22,17 +22,25 @@ export default function GoalInput({ addGoalHandler, endAddGoalHandler, visible }
                 />
                 <TextInput
                     style={styles.textInput}
-                    placeholder='Your course goal!'
+                    placeholder='Your course goal...'
                     onChangeText={goalInputHanlder}
                     value={enteredGoalText}
                 >
                 </TextInput>
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}>
-                        <Button title='Add Goal' onPress={addGoalComponentHandler} />
+                        <Button
+                            title='Add Goal'
+                            onPress={addGoalComponentHandler}
+                            color='#5e0acc'
+                        />
                     </View>
                     <View style={styles.button}>
-                        <Button title='Cancel' onPress={endAddGoalHandler} />
+                        <Button
+                            title='Cancel'
+                            onPress={endAddGoalHandler}
+                            color='#f31282'
+                        />
                     </View>
                 </View>
             </View>
@@ -55,9 +63,12 @@ const styles = StyleSheet.create({
     },
     textInput: {
         borderWidth: 1,
-        borderColor: '#cccccc',
+        borderColor: '#e4d0ff',
+        backgroundColor: '#e4d0ff',
+        color: '#120438',
+        borderRadius: 6,
         width: '100%',
-        padding: 8
+        padding: 10
     },
     buttonContainer: {
         marginTop: 16,
